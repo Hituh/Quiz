cb17ceffa3db24e2bc93274f9e3e2f3656452ca6
syncvi <ggobestie@gmail.com>
2023-01-10 00:53:26 +0100

    chyba git

---
+++ Pages/scorePage/scorePage.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { useParams } from 'react-router-dom'
+import './scorePage.css'
+
+const Score = () => {
+    const { score } = useParams();
+    return (
+        <div className="example-container11">
+            <div className="container11" >
+                <div  style={{color:"black",display: "flex", justifyContent: "center",alignItems: "center", fontSize:"50px"}}>
+                    Twój wynik
+                </div>
+                <div  style={{color:"gold",display: "flex", justifyContent: "center",alignItems: "center", fontSize:"50px"}}>
+                    {score}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Score
